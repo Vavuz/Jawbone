078b0422425516588af800ba63a82416747267f7
Marco Vavassori <marcovava2001@gmail.com>
2024-10-14 15:47:21 +0100

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-P6XKBIIY.mjs";import"./chunk-ARCPEZUO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-6QTS66HO.mjs";import"./chunk-Y6R6RS47.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
