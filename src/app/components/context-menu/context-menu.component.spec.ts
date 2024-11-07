04a0ba3733811b1abee77523f6664e0a4c5b60b2
Marco Vavassori <marcovava2001@gmail.com>
2024-11-07 19:20:32 +0000

    ContextMenuComponent tests

--- src/app/components/context-menu/context-menu.component.spec.ts
+++ src/app/components/context-menu/context-menu.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ContextMenuComponent } from './context-menu.component';
+import { By } from '@angular/platform-browser';
 
 describe('ContextMenuComponent', () => {
   let component: ContextMenuComponent;
@@ -7,16 +8,33 @@ describe('ContextMenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ContextMenuComponent]
-    })
-    .compileComponents();
-    
+      declarations: [ContextMenuComponent]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ContextMenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit editNode event when Edit Node button is clicked', () => {
+    spyOn(component.editNode, 'emit');
+
+    const editButton = fixture.debugElement.query(By.css('button:first-child')).nativeElement;
+    editButton.click();
+
+    expect(component.editNode.emit).toHaveBeenCalled();
+  });
+
+  it('should emit deleteNode event when Delete Node button is clicked', () => {
+    spyOn(component.deleteNode, 'emit');
+
+    const deleteButton = fixture.debugElement.query(By.css('button:last-child')).nativeElement;
+    deleteButton.click();
+
+    expect(component.deleteNode.emit).toHaveBeenCalled();
   });
 });
