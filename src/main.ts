f53ea416f0cce53de57d8cca98fcbe9e719dd63f
Marco Vavassori <marcovava2001@gmail.com>
2024-11-07 19:17:18 +0000

    Image export testing

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import html2canvas from 'html2canvas';
+import { HTML2CANVAS } from './app/services/image-export/html2canvas.token';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideAnimations(),
-    provideHttpClient()
+    provideHttpClient(withFetch()),
+    { provide: HTML2CANVAS, useValue: html2canvas },
   ]
 }).catch((err) => console.error(err));
